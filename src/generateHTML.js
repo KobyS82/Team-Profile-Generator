771df10df9437bb8f5e0fc4cc7819360ee7e1330
KobyS82 <kobysillito@gmail.com>
2023-02-21 22:40:36 -0600

    added some barebones

---
+++ src/generateHTML.js
@@ -0,0 +1,21 @@
+const generateManager = function (manager) {
+    return``;
+};
+
+const generateEngineer = function (engineer) {
+    return``;
+};
+
+const generateIntern = function (intern) {
+    return ``;
+};
+
+// push array to page 
+generateHTML = (data) => {};
+
+// generate html page 
+const generateTeamPage = function (employeeCards) {   
+    return``;
+};
+
+module.exports = generateHTML;
